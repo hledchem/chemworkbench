8d92163757eb5e0bf257eeaeec8c86e42f686c8b
hledchem <haydenledvina@gmail.com>
2026-01-06 13:38:19 -0800

    Add processor registration and retrieval functions

--- core/registry.py
+++ core/registry.py
@@ -1,1 +1,12 @@
+from typing import Type, List
+from .base_processor import BaseProcessor
 
+_PROCESSORS: List[Type[BaseProcessor]] = []
+
+def register(processor_cls: Type[BaseProcessor]):
+    """Register a processor class."""
+    _PROCESSORS.append(processor_cls)
+
+def get_all() -> List[Type[BaseProcessor]]:
+    """Return all registered processors."""
+    return _PROCESSORS
