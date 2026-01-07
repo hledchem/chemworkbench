f7ad7beaaaca1c2cffde4ca98ecc506f9fd95dad
hledchem <haydenledvina@gmail.com>
2026-01-06 18:19:04 -0800

    Add function to prepare UV-Vis plot data

--- processors/uvvis/plotter.py
+++ processors/uvvis/plotter.py
@@ -1,1 +1,9 @@
-
+def prepare_plot_data(data):
+    return {
+        "type": "uvvis",
+        "x": data["x"],
+        "y": data["y"],
+        "metadata": {
+            "source": data.get("source", "")
+        }
+    }
